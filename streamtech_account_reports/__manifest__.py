eb54f6b6602085a3cfdac8a915fcd1a653fba310
Jethro Guce <gucejethro@gmail.com>
2020-12-28 09:55:47 +0800

    [ADD] Initial Account Reports module

---
+++ streamtech_account_reports/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#   ACHIEVE WITHOUT BORDERS
+#
+##############################################################################
+{
+    'name': "Streamtech Account Reports",
+    'summary': """
+        Streamtech Account Reports Customization
+        """,
+    'description': """
+        Streamtech Account Reports
+    """,
+    'author': "Achieve Without Borders, Inc",
+    'website': "http://www.achievewithoutborders.com",
+    'category': 'Approval',
+    'version': '13.0.1.0.0',
+    'depends': ['account_accountant'],
+    'data': [
+        
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False
+}
