e0db479645fdc4b07afbbeb1fe2b887d1e9b2835
Jethro Guce <gucejethro@gmail.com>
2020-12-28 09:55:18 +0800

    [REF] Change category

--- streamtech_journal_voucher/__manifest__.py
+++ streamtech_journal_voucher/__manifest__.py
@@ -14,7 +14,7 @@
     """,
     'author': "Achieve Without Borders, Inc",
     'website': "http://www.achievewithoutborders.com",
-    'category': 'Approval',
+    'category': 'Report',
     'version': '13.0.1.0.0',
     'depends': ['account'],
     'data': [
