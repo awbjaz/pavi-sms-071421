6ce679bf4d57b049cd6ad480d57f8302bf547f8d
Jethro Guce <gucejethro@gmail.com>
2021-02-08 11:50:57 +0800

    [REL] SF Connector python dependencies

--- salesforce_connector/__manifest__.py
+++ salesforce_connector/__manifest__.py
@@ -13,12 +13,14 @@
     'price': 400,
     'currency': 'EUR',
     'license': 'OPL-1',
-
     'data': [
         'security/ir.model.access.csv',
         'views/views.xml',
         'data/schedule.xml',
     ],
+    'external_dependencies': {
+        'python': ['cryptography', 'simple_salesforce'],
+    },
     'installable': True,
     'application': True,
 }
