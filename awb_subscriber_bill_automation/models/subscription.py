7fbbaa9f9e6316550ea7e8acba564510e16f0301
Jethro Guce <gucejethro@gmail.com>
2021-02-08 14:10:35 +0800

    [REL] Updates and fixes

--- awb_subscriber_bill_automation/models/subscription.py
+++ awb_subscriber_bill_automation/models/subscription.py
@@ -30,9 +30,19 @@ class SaleSubscription(models.Model):
     def _prepare_renewal_order_values(self):
         res = super(SaleSubscription, self)._prepare_renewal_order_values()
         if self.opportunity_id:
+            plan = []
+            for line in self.opportunity_id.plan.sale_order_template_line_ids:
+                data = {
+                    'product_id': line.product_id.id,
+                    'name': line.name,
+                    'product_uom_qty': line.product_uom_qty,
+                    'price_unit': line.price_unit,
+                }
+                plan.append((0, 0, data))
             res[self.id].update({
                 'opportunity_id': self.opportunity_id.id,
                 'sale_order_template_id': self.opportunity_id.plan.id,
+                'order_line': plan,
             })
         _logger.debug(f'Result {res}')
         return res
@@ -42,7 +52,6 @@ class SaleSubscription(models.Model):
         if self.opportunity_id:
             order = self.env['sale.order'].search([('id', '=', res['res_id'])])
             order.action_confirm()
-        _logger.debug(f'Result Order order{order}')
         _logger.debug(f"Result Order {res['res_id']}")
         _logger.debug(f'Result Renewal {res}')
         return res
