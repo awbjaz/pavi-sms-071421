45e3590e3c13dd773ce36139ccbfe58acb28f778
Jethro Guce <gucejethro@gmail.com>
2021-02-16 11:36:25 +0800

    [IMP] SOA Printing

--- awb_subscriber_bill/models/account.py
+++ awb_subscriber_bill/models/account.py
@@ -16,13 +16,18 @@ _logger = logging.getLogger(__name__)
 class AccountMove(models.Model):
     _inherit = "account.move"
 
-    statement_line_ids = fields.One2many('account.statement.line', 'move_id', string="Statement Line")
+    statement_line_ids = fields.One2many(
+        'account.statement.line', 'move_id', string="Statement Line")
     atm_ref = fields.Char(string="ATM Reference")
     start_date = fields.Date(string="Start Date")
     end_date = fields.Date(string="End Date")
     period_covered = fields.Date(string="Period Covered")
-    total_statement_balance = fields.Float(string="Total Statement Balance", compute='_compute_statement_balance')
-    is_subscription = fields.Boolean(string="Is Subscribtion", compute="_compute_is_subscription")
+    total_statement_balance = fields.Float(
+        string="Total Statement Balance", compute='_compute_statement_balance')
+    is_subscription = fields.Boolean(
+        string="Is Subscribtion", compute="_compute_is_subscription")
+    total_vat = fields.Float(
+        string="Total Vat", compute='_compute_statement_balance')
 
     @api.depends('invoice_line_ids')
     def _compute_is_subscription(self):
@@ -32,15 +37,18 @@ class AccountMove(models.Model):
                 for line in rec.invoice_line_ids:
                     if line.subscription_id:
                         rec.is_subscription = True
-                    
+
     @api.depends('statement_line_ids')
     def _compute_statement_balance(self):
         for rec in self:
             rec.total_statement_balance = sum(
                 rec.statement_line_ids.mapped('amount'))
+            rec.total_vat = sum(
+                rec.statement_line_ids.filtered(lambda r: r.statement_type == 'vat').mapped('amount'))
 
     def action_cron_generate(self):
-        records = self.env['account.move'].search([('type', 'in', ['out_invoice', 'in_invoice'])])
+        records = self.env['account.move'].search(
+            [('type', 'in', ['out_invoice', 'in_invoice'])])
         _logger.debug(f'Record Invoice and Bill {records}')
 
         for rec in records:
@@ -62,7 +70,7 @@ class AccountMove(models.Model):
             code = ''
             if rec.partner_id.subscriber_location_id.code:
                 code = rec.partner_id.subscriber_location_id.code + '-'
-            
+
             if rec.invoice_date_due:
                 ref_date = '-' + rec.invoice_date_due.strftime('%m%d')
             else:
@@ -73,15 +81,28 @@ class AccountMove(models.Model):
             lines = []
             # invoice Lines
             for line in rec.invoice_line_ids:
-                data = {
-                    'name': line.name,
-                    'amount': line.credit,
-                }
-                lines.append((0, 0, data))
+                if line.product_id.product_tmpl_id.id == self.env.ref('awb_subscriber_product_information.product_device_fee').id:
+                    data = {
+                        'name': line.name,
+                        'statement_type': 'device_fee',
+                        'amount': line.credit,
+                    }
+                    _logger.debug(
+                        f'Prod ID temp {line.product_id.product_tmpl_id.id}')
+                    lines.append((0, 0, data))
+                else:
+                    data = {
+                        'name': line.name,
+                        'statement_type': 'subs_fee',
+                        'amount': line.credit,
+                    }
+                    lines.append((0, 0, data))
+
             # taxes
             for line in rec.line_ids:
                 if line.tax_line_id:
-                    data = {'name': "Value Added Tax"}
+                    data = {'name': "Value Added Tax",
+                            'statement_type': 'vat'}
                     if line.tax_line_id.type_tax_use == 'sale':
                         data['amount'] = line.credit
                     elif line.tax_line_id.type_tax_use == 'purchase':
@@ -98,6 +119,7 @@ class AccountMove(models.Model):
 
                 data = {
                     'name': 'Payment',
+                    'statement_type': 'payment',
                     'amount': total_amount * -1,
                 }
 
@@ -110,11 +132,14 @@ class AccountMove(models.Model):
                     total_amount += (line.amount_total - line.amount_residual)
                 data = {
                     'name': 'Credits/Rebates',
+                    'statement_type': 'credit',
                     'amount': total_amount * -1,
                 }
 
                 lines.append((0, 0, data))
-
+            prod_id = self.env.ref(
+                'awb_subscriber_product_information.product_device_fee').id
+            _logger.debug(f'Prod ID {prod_id}')
             _logger.debug(f'Inv Lines {rec.name}{lines}')
             rec.update({'statement_line_ids': None})
             rec.update({'statement_line_ids': lines})
@@ -125,5 +150,11 @@ class AccountStatementLine(models.Model):
 
     name = fields.Char(string="Description")
     amount = fields.Float(string="Amount")
+    statement_type = fields.Selection([('subs_fee', 'Subscription Fee'),
+                                       ('device_fee', 'Device Fee'),
+                                       ('vat', 'VAT'),
+                                       ('payment', 'Payment'),
+                                       ('credit', 'Credit'),
+                                       ('other', 'Other')], string="Statement Type")
 
     move_id = fields.Many2one('account.move', string="Invoice")
