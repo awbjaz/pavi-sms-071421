dc56e506242536cd2cb475277322eb3968261646
Jethro Guce <gucejethro@gmail.com>
2021-02-25 23:49:52 +0800

    [REL] PO level approval

--- awb_purchase_order_approval/models/purchase.py
+++ awb_purchase_order_approval/models/purchase.py
@@ -11,51 +11,81 @@ class PurchaseOrder(models.Model):
     _inherit = "purchase.order"
 
     state = fields.Selection(selection_add=[('for_approval', 'For Approval')])
-    requested_by = fields.Many2one('res.partner', default=lambda self: self.env.user.id)
-    reviewed_by = fields.Many2one('res.partner')
-    approval_lines = fields.One2many('purchase.order.approval.line', 'order_id', string='Approval Lines', tracking=True, readonly=True, copy=True)
+    requested_by = fields.Many2one('res.partner', default=lambda self: self.env.user.id, required=True)
+    reviewed_by = fields.Many2one('res.partner', required=True)
+    approval_lines = fields.One2many('purchase.order.approval.line', 'order_id',
+                                     string='Approval Lines', tracking=True, copy=True)
     can_approve = fields.Boolean(compute='_compute_can_approve', default=False)
+
+    index_seq = fields.Integer(string='Index Sequence', default=1)
 
     def action_for_approval(self):
         self.state = 'for_approval'
-        args = [('is_active', '=', True),
+        args = [('active', '=', True),
                 ('min_amount', '<=', self.amount_total),
                 ('max_amount', '>', self.amount_total),
                 ]
         approval_rule = self.env['purchase.order.approval'].search(args, limit=1)
+
         approvers = []
-        for approver in approval_rule.approved_by:
-            data = {
-                'rule_id': approval_rule.id,
-                'approver_id': approver.id
-            }
-            approvers.append((0, 0, data))
+        for record in approval_rule.approver_ids:
+            for approver in record.approved_by:
+                data = {
+                    'rule_id': approval_rule.id,
+                    'approval_condition': record.approval_condition,
+                    'sequence': record.sequence,
+                    'approver_id': approver.id
+                }
+                approvers.append((0, 0, data))
 
         self.sudo().update({'approval_lines': [(5, 0, 0)]})
         self.sudo().update({'approval_lines': approvers})
 
     def action_approve(self):
         is_approved = False
-        approvers = []
+        is_validate = False
         approval_condition = 'and'
         approval_status = []
-        for approval in self.approval_lines:
+        approvers = []
+        args = [('state', '=', 'pending'),
+                ('order_id', '=', self.id),
+                ('sequence', '=', self.index_seq)]
+
+        approval_line_data = self.env['purchase.order.approval.line'].search(args)
+
+        for approval in approval_line_data:
             approver = approval.approver_id.id
             approvers.append(approver)
             if approver == self.env.user.id:
-                approval_condition = approval.rule_id.approval_condition
+                approval_condition = approval.approval_condition
                 approval.state = 'approved'
+                approval.can_proceed = True
             approval_status.append(approval.state)
 
-        if len(approvers) <= 1:
+        if approval_condition == 'or':
             is_approved = True
-        else:
-            if approval_condition == 'or':
-                is_approved = True
-            elif approval_condition == 'and':
-                is_approved = all([state == 'approved' for state in approval_status])
+            for rec in approval_line_data:
+                rec.can_proceed = True
 
+            is_validate = all([line.can_proceed == True for line in self.approval_lines])
+            _logger.debug(f'IS APPROVED IN CONDITION: {is_approved}')
+            
+        elif approval_condition == 'and':
+            is_approved = all([state == 'approved' for state in approval_status])
+            is_validate = all([line.can_proceed == True for line in self.approval_lines])
+
+        _logger.debug(f'IS APPROVED: {is_approved}')
+        _logger.debug(f'IS APPROVED Status: {approval_status}')
+        _logger.debug(f'IS APPROVED COnditon: {approval_condition}')
+        _logger.debug(f'IS VALIDATED: {is_validate}')
         if is_approved:
+            self.index_seq += 1
+
+            if approval_line_data:
+                is_approved = False
+                approval_status.clear()
+
+        if is_validate:
             self.state = 'sent'
             self.reviewed_by = self.env.user.id
             self.button_confirm()
@@ -75,9 +105,15 @@ class PurchaseOrder(models.Model):
             self.sudo().update({'can_approve': False})
             if self.state == 'for_approval':
                 can_approve = False
-                for approval in self.approval_lines:
+                args = [('state', '=', 'pending'),
+                        ('order_id', '=', self.id),
+                        ('sequence', '=', self.index_seq)]
+
+                approval_line_data = self.env['purchase.order.approval.line'].search(args)
+
+                for approval in approval_line_data:
                     approver = approval.approver_id.id
-                    _logger.debug(f'{approver} {approval.state}')
+                    _logger.debug(f'_compute_can_approve: {approver} {approval.state}')
                     if approver == self.env.user.id and approval.state == 'pending':
                         can_approve = True
 
