bad4ce0da74e89b73b435ab2052021b066711944
Jethro Guce <gucejethro@gmail.com>
2021-03-10 15:57:42 +0800

    [REL] Subscriber bill

--- awb_subscriber_bill/__manifest__.py
+++ awb_subscriber_bill/__manifest__.py
@@ -28,7 +28,6 @@
     'data': [
         'security/ir.model.access.csv',
         'data/account_move_atm_ref_sequence.xml',
-        'data/account_data.xml',
         # 'views/assets.xml',
         'views/account_view.xml',
         'views/account_printer_data.xml',
