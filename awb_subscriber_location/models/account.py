c7d5f3ce94afc5c9923625f713e2a84c2930986a
Jethro Guce <gucejethro@gmail.com>
2021-03-11 00:44:03 +0800

    [REL] Improvements

--- awb_subscriber_location/models/account.py
+++ awb_subscriber_location/models/account.py
@@ -22,9 +22,13 @@ class AccountMove(models.Model):
         today = fields.Date.today()
         account_moves = self.env['account.move']
         moves = account_moves.search(
-            [('state', '=', 'draft'), ('posting_date', '<=', today)])
+            [('state', '=', 'draft'), ('posting_date', '<=', today)], limit=200)
         _logger.debug(f'Record {moves}')
 
         for rec in moves:
-            rec.action_approve()
-            rec.action_post()
+            try:
+                rec.action_approve()
+                rec.action_post()
+                self.env.cr.commit()
+            except Exception as e:
+                _logger.info(f'Error {e}')
