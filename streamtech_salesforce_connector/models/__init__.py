44e8fdebd24ac644e2fe14b2988dab13c56b594e
Jethro Guce <gucejethro@gmail.com>
2021-03-12 10:08:41 +0800

    [REL] Fix for contacts

--- streamtech_salesforce_connector/models/__init__.py
+++ streamtech_salesforce_connector/models/__init__.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
 from . import custom_lead
-from . import partner
 from . import salesforce_importer_products
 from . import salesforce_importer_opportunities
 from . import salesforce_importer_customers
