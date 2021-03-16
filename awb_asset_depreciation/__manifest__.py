d2eb9c76c79fb0c786aa16fa49edcf329a2b0af5
Ron Michael Diokno Khu <ron.khu@achievewithoutborders.com>
2021-03-16 17:25:17 +0800

    [ADD] Asset depreciation module

---
+++ awb_asset_depreciation/__manifest__.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#   ACHIEVE WITHOUT BORDERS
+#
+##############################################################################
+{
+    'name': "AWB Asset Depreciation",
+
+    'summary': """
+        Asset Depreciation.
+        """,
+
+    'description': """
+        Extension Odoo Apps
+    """,
+
+    'author': "Achieve Without Borders",
+
+    'license': 'LGPL-3',
+
+    'category': 'Accounting/Accounting',
+
+    'version': '13.0.1.0.0',
+
+    'depends': ['account_asset'],
+
+    'data': [
+        'security/ir.model.access.csv',
+        'views/account_asset_view.xml'
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        # 'demo/demo.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False
+
+}
---
+++ awb_asset_depreciation/models/account_asset.py
@@ -0,0 +1,51 @@
+import calendar
+import datetime
+
+from odoo import api, fields, models, _
+
+import logging
+_logger = logging.getLogger(__name__)
+
+class AccountAsset(models.Model):
+    _inherit = 'account.asset'
+    
+    first_depreciation_date  = fields.Date(string="First Depreciation Date", readonly=True, required=True, stored=True, default=datetime.datetime.today())
+
+    def compute_depreciation_date(self, acquisition_date):
+        if acquisition_date:
+            day = acquisition_date.day
+            month = acquisition_date.month
+            year = acquisition_date.year
+
+            if day > 15:
+                month = month + 1
+
+            day = calendar.monthrange(year, month)[1]
+
+            return datetime.date(year, month, day)
+
+    @api.model   
+    def create(self, vals):
+        _logger.error('create')
+        # if vals.has_key('acquisition_date'):
+        #     vals['first_depreciation_date'] = self.compute_depreciation_date(vals['acquisition_date'])
+    
+        vals['first_depreciation_date'] = self.compute_depreciation_date(vals['acquisition_date'])
+
+        result = super(AccountAsset, self).create(vals)
+        return result 
+
+    @api.onchange('acquisition_date')
+    def onchange_acquisition_date(self):
+        _logger.error('onchange_acquisition_date')
+        if self.acquisition_date:
+            self.first_depreciation_date = self.compute_depreciation_date(self.acquisition_date)
+
+    @api.onchange('first_depreciation_date')
+    def onchange_first_depreciation_date(self):
+        _logger.error('onchange_first_depreciation_date')
+        if self.acquisition_date:
+            expected = self.compute_depreciation_date(self.acquisition_date)
+            if self.first_depreciation_date != expected:
+                self.first_depreciation_date = expected 
+                           
