14b56e8fa59d24019c98628425e9a5b899b2669d
Ron Michael Khu <ron.khu@achievewithoutborders.com>
2021-03-21 14:35:01 +0000

    [IMP] Add autocompute for first depreciation date

--- awb_asset_depreciation/models/account_asset.py
+++ awb_asset_depreciation/models/account_asset.py
@@ -8,30 +8,42 @@ _logger = logging.getLogger(__name__)
 
 class AccountAsset(models.Model):
     _inherit = 'account.asset'
-    
-    first_depreciation_date  = fields.Date(string="First Depreciation Date", readonly=True, required=True, stored=True, default=datetime.datetime.today())
+
+    first_depreciation_date  = fields.Date(string="First Depreciation Date", readonly=True, required=True, store=True, compute='_default_first_depreciation_date')
+
+    # trigger_first_depreciation_date  = fields.Date(string="First Depreciation Date", readonly=True, store=False, compute='_default_trigger_first_depreciation_date', default=datetime.datetime.now())
+
+    # @api.depends("trigger_first_depreciation_date", "acquisition_date")
+    # @api.depends("acquisition_date")
+    def _default_first_depreciation_date(self):
+        _logger.error('_default_first_depreciation_date')
+        return self.compute_depreciation_date(self.acquisition_date) 
+
+    def _default_trigger_first_depreciation_date(self):
+        _logger.error('_default_trigger_first_depreciation_date')
+        return self.compute_depreciation_date(self.acquisition_date) 
 
     def compute_depreciation_date(self, acquisition_date):
-        if acquisition_date:
-            day = acquisition_date.day
-            month = acquisition_date.month
-            year = acquisition_date.year
+        _logger.error('compute_depreciation_date')
+        base_date = acquisition_date
+        if base_date is None or base_date is False or base_date is True:
+            base_date = datetime.datetime.now()
 
-            if day > 15:
-                month = month + 1
+        day = base_date.day
+        month = base_date.month
+        year = base_date.year
 
-            day = calendar.monthrange(year, month)[1]
+        if day > 15:
+            month = month + 1
 
-            return datetime.date(year, month, day)
+        day = calendar.monthrange(year, month)[1]
 
-    @api.model   
+        return datetime.date(year, month, day)
+
+    @api.model
     def create(self, vals):
         _logger.error('create')
-        try:
-            if vals.has_key('acquisition_date'):
-                vals['first_depreciation_date'] = self.compute_depreciation_date(vals['acquisition_date'])
-        except:
-            pass #evil
+        vals['first_depreciation_date'] = self.compute_depreciation_date(vals.get('acquisition_date', datetime.datetime.now()))
 
         result = super(AccountAsset, self).create(vals)
         return result 
@@ -39,14 +51,12 @@ class AccountAsset(models.Model):
     @api.onchange('acquisition_date')
     def onchange_acquisition_date(self):
         _logger.error('onchange_acquisition_date')
-        if self.acquisition_date:
-            self.first_depreciation_date = self.compute_depreciation_date(self.acquisition_date)
+        self.first_depreciation_date = self.compute_depreciation_date(self.acquisition_date)
 
     @api.onchange('first_depreciation_date')
     def onchange_first_depreciation_date(self):
         _logger.error('onchange_first_depreciation_date')
-        if self.acquisition_date:
-            expected = self.compute_depreciation_date(self.acquisition_date)
-            if self.first_depreciation_date != expected:
-                self.first_depreciation_date = expected 
+        expected = self.compute_depreciation_date(self.acquisition_date)
+        if self.first_depreciation_date != expected:
+            self.first_depreciation_date = expected 
                            
