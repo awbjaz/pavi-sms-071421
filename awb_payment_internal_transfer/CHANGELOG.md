da8760d898f964f63a554ff773b1250e67aff7a3
Ron Michael Khu <ron.khu@achievewithoutborders.com>
2021-03-29 06:17:37 +0000

    [IMP] Payment Internal Transfer: change Source to Destination display name

---
+++ awb_payment_internal_transfer/CHANGELOG.md
@@ -0,0 +1,3 @@
+# v1.1.0 - 2021-03-26
+
+- change Source Payment to Destination Payment Display name
--- awb_payment_internal_transfer/__manifest__.py
+++ awb_payment_internal_transfer/__manifest__.py
@@ -14,7 +14,7 @@
     'author': "Achieve Without Borders, Inc",
     'website': "http://www.achievewithoutborders.com",
     'category': 'Accounting/Payments',
-    'version': '13.0.1.0.0',
+    'version': '13.0.1.1.0',
     'depends': ['account'],
     'data': [
         'security/ir.model.access.csv',
