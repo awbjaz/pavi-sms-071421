37f9919abeaaf1088ffb29b2aa4a5c9f33b0b854
Ron Michael Khu <ron.khu@achievewithoutborders.com>
2021-04-22 10:00:43 +0000

    [FIX] Account Move Batch: duplicate and wrong company when generate invoices

--- awb_account_move_batch/__manifest__.py
+++ awb_account_move_batch/__manifest__.py
@@ -14,7 +14,7 @@
     'author': "Achieve Without Borders, Inc",
     'website': "http://www.achievewithoutborders.com",
     'category': 'Accounting/Accounting',
-    'version': '13.0.1.2.2',
+    'version': '13.0.1.3.3',
     'depends': ['account', 'awb_subscriber_location'],
     'data': [
         'data/account_move_rebate_sequence.xml',
