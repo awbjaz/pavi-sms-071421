84b3ba715c2fcd0b99fd27b0cbbaee4962a95785
Ron Michael Khu <ron.khu@achievewithoutborders.com>
2021-04-29 12:28:57 +0000

    [IMP] HR Expense Approval: Add Corporate Logo in Module and Update Summary [FIX] Requisition Purchase: only 1 vendor in draft PO process

--- requisition_purchase/__manifest__.py
+++ requisition_purchase/__manifest__.py
@@ -21,7 +21,7 @@
 
     'category': 'Localization',
 
-    'version': '13.0.1.3.0',
+    'version': '13.0.1.3.1',
 
     'depends': ['requisition_approval', 'purchase_requisition', 'purchase', 'stock', 'purchase_stock'],
 
