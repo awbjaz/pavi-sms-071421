6b11253d21e89011b9647ea2d331ecd39a8749f8
Aeriane Charmaine Dizon <aeriane.dizon@achievewithoutborders.com>
2021-07-09 18:43:12 +0800

    updated manifest

--- awb_aradial_connector/__manifest__.py
+++ awb_aradial_connector/__manifest__.py
@@ -16,7 +16,7 @@
     'website': "http://www.achievewithoutborders.com",
     'category': 'Sales',
     'version': '13.0.1.0.0',
-    'depends': ['crm', 'sale_management', 'sale_subscription', 'awb_subscriber_product_information'],
+    'depends': ['base', 'crm', 'sale_management', 'sale_subscription', 'awb_subscriber_product_information'],
     'data': [],
     'installable': True,
     'application': False,
