9027bbecf45450fc69852b9799f58969983149fb
Aeriane Charmaine Dizon <aeriane.dizon@achievewithoutborders.com>
2021-07-09 18:23:55 +0800

    Initialized aradial_connector

---
+++ awb_aradial_connector/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#   ACHIEVE WITHOUT BORDERS
+#
+##############################################################################
+{
+    'name': "AWB Aradial Connector",
+    'summary': """
+        Odoo - Aradial Connector
+        """,
+    'description': """
+        Odoo - Aradial Connector
+    """,
+    'author': "Achieve Without Borders, Inc",
+    'website': "http://www.achievewithoutborders.com",
+    'category': 'Sales',
+    'version': '13.0.1.0.0',
+    'depends': ['crm', 'sale_management', 'sale_subscription', 'awb_subscriber_product_information'],
+    'data': [],
+    'installable': True,
+    'application': False,
+    'auto_install': False
+}
---
+++ awb_aradial_connector/models/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+
+from . import awb_aradial_connector
+from . import subscription
