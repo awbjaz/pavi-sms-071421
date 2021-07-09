6dc02f09dbdc1dbeb3ef27a46605c6b2c8feadff
Aeriane Charmaine Dizon <aeriane.dizon@achievewithoutborders.com>
2021-07-09 20:24:50 +0800

    fixed variable for name

--- awb_aradial_connector/models/subscription.py
+++ awb_aradial_connector/models/subscription.py
@@ -32,7 +32,7 @@ class Subscription(models.Model):
             for line_id in record.recurring_invoice_line_ids:
                 products += line_id.name + " "
             first_name = record.name.split()[0]
-            last_name = record.name.split()[1]
+            last_name = record.partner_id.name.split()[1]
 
             self.data = {
                 'UserID': record.code,
