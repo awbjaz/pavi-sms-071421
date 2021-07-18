bd480c5a932c3fec6a97b32c3d81ef2c02afe9a3
Chriselda Amado <ccamado@novare.com.hk>
2021-07-18 20:51:51 +0800

    [FIX] User Access Issue when non Purchase User creates Purchase Requisition

--- requisition_purchase/__manifest__.py
+++ requisition_purchase/__manifest__.py
@@ -21,7 +21,7 @@
 
     'category': 'Localization',
 
-    'version': '13.0.1.4.1',
+    'version': '13.0.1.4.2',
 
     'depends': ['requisition_approval', 'purchase_requisition', 'purchase', 'stock', 'purchase_stock'],
 
