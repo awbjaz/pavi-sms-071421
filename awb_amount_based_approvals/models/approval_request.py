a76a4287f6b42b71018d928d9a7be487296f82b1
Raynier Portus <raynier.portus@achievewithoutborders.com>
2021-07-22 15:16:18 +0800

    [ADD] Sequence for List of Approvers

--- awb_amount_based_approvals/models/approval_request.py
+++ awb_amount_based_approvals/models/approval_request.py
@@ -16,6 +16,7 @@ class ApprovalRequest(models.Model):
     def create(self, vals):
         
         data = []
+        seq = 0
 
         res = super(ApprovalRequest, self).create(vals)
 
@@ -32,21 +33,25 @@ class ApprovalRequest(models.Model):
 
         category_id = res.mapped('category_id').rule_ids.filtered(lambda x: x.id == rule_id.id)
         if category_id.manager_id:
-            if manager_id:    
+            if manager_id:
+                seq += 1    
                 manager_data = {
                         'user_id': manager_id,
                         'status': 'new',
                         'request_id': res.id,
+                        'sequence': seq ,
                     }
                 data.append(manager_data)
             else:
                 raise UserError(_('No User Account Associated on this Manager.'))
         if category_id.operation_head_id:
             if head_id:
+                seq += 1
                 head_data = {
                         'user_id': head_id,
                         'status': 'new',
                         'request_id': res.id,
+                        'sequence': seq ,
                 }
                 data.append(head_data)
             else:
@@ -66,3 +71,50 @@ class ApprovalRequest(models.Model):
             _logger.debug('Approvals Created!')
         return res
 
+    @api.depends('approver_ids.status')
+    def _compute_request_status(self):
+        for request in self:
+            status_lst = request.mapped('approver_ids.status')
+
+            if status_lst:
+                if status_lst.count('cancel'):
+                    status = 'cancel'
+                elif status_lst.count('refused'):
+                    status = 'refused'
+                elif status_lst.count('new') == len(status_lst):
+                    status = 'new'
+                elif status_lst.count('approved') == len(status_lst):
+                    status = 'approved'
+                else:
+                    status = 'pending'
+            else: 
+                status = 'new'
+            request.request_status = status
+
+    def action_confirm(self):
+
+        res = super(ApprovalRequest, self).action_confirm()
+        request_approver = self.mapped('approver_ids')
+        for record in request_approver:
+            if record.sequence:
+                approvers = request_approver.filtered(lambda approver: approver.sequence != 1)
+                approvers.write({'status': 'new'})
+        return res
+
+    def action_approve(self):
+
+        res = super(ApprovalRequest,self).action_approve()  
+        approver = self.mapped('approver_ids')
+        current_approver = approver.filtered(lambda approver: approver.user_id == self.env.user)
+        next_approver = approver.filtered(lambda approver: approver.sequence == current_approver.sequence + 1)
+        if next_approver:
+            if next_approver.sequence:
+                next_approver.write({'status': 'pending'})
+      
+        return res
+
+class ApprovalApprover(models.Model):
+    _inherit = 'approval.approver'
+
+    sequence = fields.Integer(string='Sequence', default=1, readonly=True)
+    approval_condition = fields.Selection([('and', 'AND'), ('or', 'OR')], string='Condition', default='and')
