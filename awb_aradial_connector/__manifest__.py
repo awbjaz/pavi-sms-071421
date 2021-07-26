7ae09f60ba808e033fe70345928ea5763038ad9a
Jasper Rivera <jasper.rivera@achievewithoutborders.com>
2021-07-26 17:50:48 +0800

    [IMP] TMS Welcome notification new subs

--- awb_aradial_connector/__manifest__.py
+++ awb_aradial_connector/__manifest__.py
@@ -15,9 +15,12 @@
     'author': "Achieve Without Borders, Inc",
     'website': "http://www.achievewithoutborders.com",
     'category': 'Sales',
-    'version': '13.0.1.0.0',
+    'version': '13.0.1.0.1',
     'depends': ['base', 'crm', 'sale_management', 'sale_subscription', 'awb_subscriber_product_information'],
-    'data': [],
+    'data': [
+        'data/sale_subscription_data.xml',
+        'data/awb_sms_template_data.xml',
+    ],
     'installable': True,
     'application': False,
     'auto_install': False
